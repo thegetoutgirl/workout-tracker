c3672c1f449a564abb1d067579c6e69fc8fb682f
thegetoutgirl <amyahaha@gmail.com>
2020-03-27 23:38:18 -0400

    updated database info...will it work?

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { Mongoose } from "mongoose";
 
 // If deployed, use the deployed database. Otherwise use local "mongoHeadlines" database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongodb://Amy:test123@ds113746.mlab.com:13746/heroku_pm6jt3wm";
 
 // Connect to the MongoDB
 Mongoose.connect(MONGODB_URI);
