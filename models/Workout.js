2f7468cc824eb7b0ba78a9676abffc2ff38a87d2
thegetoutgirl <amyahaha@gmail.com>
2020-03-30 17:13:11 -0400

    server issues working

--- models/Workout.js
+++ models/Workout.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema ({
-    day: date,
     exercises: [
        { 
            type: Schema.Types.ObjectId,
--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-import { Mongoose } from "mongoose";
+// import { Mongoose } from "mongoose";
 
 // If deployed, use the deployed database. Otherwise use local "mongoHeadlines" database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongodb://Amy:test123@ds113746.mlab.com:13746/heroku_pm6jt3wm";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://Amy:test123@ds113746.mlab.com:13746/heroku_pm6jt3wm";
 
 // Connect to the MongoDB
-Mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
